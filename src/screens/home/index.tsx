d7f63007bf959f6a5b86780e62b3d3a4621c1ee9
José Vieira da Silva Neto <josevieira2029@gmail.com>
2024-04-30 10:29:19 -0300

    Iniciando

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,11 @@
+import {useState} from 'react';
+import {View, Text, TextInput, Alert} from 'react-native';
+import { styles } from './styles';
+
+export function Home () {
+    return (
+        <View style={styles.container}>
+            <Text> Outro nome</Text>
+        </View>
+    )
+}
---
+++ src/screens/home/styles.ts
@@ -0,0 +1,11 @@
+// styles.ts
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 75,
+    backgroundColor: '#eae1fd'
+  },
+});
